edfad6381b53219b246a59a15064fdfa95998460
Jason Lingle <jason@lin.gl>
2013-12-27 20:49:52 -0700

    Initial implementation of back-end for new terrain rendering system.

---
+++ src/render/terrabuff.c
@@ -0,0 +1,303 @@
+/*-
+ * Copyright (c) 2013 Jason Lingle
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "../coords.h"
+#include "../alloc.h"
+#include "../defs.h"
+#include "../frac.h"
+#include "../graphics/canvas.h"
+#include "../graphics/linear-paint-tile.h"
+#include "../graphics/perspective.h"
+#include "context.h"
+#include "terrabuff.h"
+
+#define TEXSZ 256
+#define TEXMASK (TEXSZ-1)
+
+static canvas_pixel texture[TEXSZ*TEXSZ];
+
+void terrabuff_init(void) {
+  static const canvas_pixel pallet[] = {
+    argb(255,255,255,255),
+    argb(255,254,254,254),
+    argb(255,252,252,252),
+    argb(255,248,248,248),
+    argb(255,240,240,240),
+    argb(255,224,224,224),
+    argb(255,192,192,192),
+    argb(255,128,128,128),
+    argb(255,64,64,64),
+    argb(255,0,0,0),
+  };
+
+  linear_paint_tile_render(texture, TEXSZ, TEXSZ,
+                           TEXSZ/4, 1,
+                           pallet, lenof(pallet));
+}
+
+/**
+ * Tracks the boundaries of relevant slices at a given scan. The low index is
+ * the last point to the left of the canvas, whereas the right index is one
+ * past the first point to the right of the drawable area.
+ */
+typedef struct {
+  terrabuff_slice low, high;
+} scan_boundary;
+
+struct terrabuff_s {
+  /**
+   * The capacity of this buffer.
+   */
+  terrabuff_slice scap;
+  /**
+   * While performing a scan, the current slice being scanned.
+   */
+  terrabuff_slice scurr;
+  /**
+   * The offset of the slice number that the client believes it is
+   * using. Internall, all slices are zero-based from this offset, allowing
+   * point memory to always be contiguous.
+   */
+  terrabuff_slice soff;
+
+  /**
+   * The current scan index.
+   */
+  unsigned scan;
+  /**
+   * Boundaries calculated on each scan performed. Indexed by scan. Part of the
+   * same memory allocation as the terrabuff itself.
+   */
+  scan_boundary*restrict boundaries;
+  /**
+   * Points written into the terrabuff. Indexed by (scan*scap+scurr). Part of
+   * the same memory allocation as the terrabuff itself.
+   */
+  vo3*restrict points;
+};
+
+terrabuff* terrabuff_new(terrabuff_slice scap, unsigned scancap) {
+  terrabuff* this = xmalloc(sizeof(terrabuff) +
+                            sizeof(scan_boundary) * scancap +
+                            sizeof(vo3) * scap * scancap);
+  this->scap = scap;
+  this->boundaries = (scan_boundary*)(this + 1);
+  this->points = (vo3*)(this->boundaries + scancap);
+
+  return this;
+}
+
+void terrabuff_delete(terrabuff* this) {
+  free(this);
+}
+
+void terrabuff_clear(terrabuff* this, terrabuff_slice l, terrabuff_slice r) {
+  this->scan = 0;
+  this->scurr = 0;
+  this->soff = l;
+}
+
+void terrabuff_next(terrabuff* this, terrabuff_slice* l, terrabuff_slice* r) {
+  this->scurr = this->boundaries[this->scan].low;
+  *l = (this->boundaries[this->scan].low  + this->soff) & (this->scap-1);
+  *r = (this->boundaries[this->scan].high + this->soff) & (this->scap-1);
+  ++this->scan;
+}
+
+void terrabuff_put(terrabuff* this, const vo3 where, coord_offset xmax) {
+  /* Update boundaries */
+  if (where[0] < 0)
+    this->boundaries[this->scan].low = this->scurr;
+  else if (where[0] < xmax)
+    this->boundaries[this->scan].high = this->scurr + 2;
+
+  memcpy(this->points[this->scan*this->scap + this->scurr], where, sizeof(vo3));
+  ++this->scurr;
+}
+
+typedef struct {
+  coord_offset y, z;
+} screen_yz;
+
+static void interpolate(screen_yz*restrict dst, vo3*restrict points,
+                        coord_offset xmax) {
+  /* TODO: Better interpolation than linear (ie, cubic) */
+  coord_offset x0 = points[1][0], x1 = points[2][0];
+  coord_offset y0 = points[1][1], y1 = points[2][1];
+  coord_offset z0 = points[1][2], z1 = points[2][2];
+  coord_offset xl = (x0 >= 0? x0 : 0), xh = (x1 < xmax? x1 : xmax);
+  coord_offset dx = x1 - x0;
+  coord_offset x;
+  fraction idx = fraction_of(dx);
+
+  for (x = xl; x <= xh; ++x) {
+    dst[x].y = fraction_smul(y0*(x1-x) + y1*(x-x0), idx);
+    dst[x].z = fraction_smul(z0*(x1-x) + z1*(x0-x), idx);
+  }
+}
+
+static void interpolate_all(screen_yz*restrict dst,
+                            vo3*restrict points,
+                            unsigned num_points,
+                            coord_offset xmax) {
+  unsigned i;
+
+  for (i = 1; i < num_points-1; ++i)
+    interpolate(dst, points + (i-1), xmax);
+}
+
+#define LINE_COLOUR (argb(255,12,12,12))
+static void draw_line(canvas*restrict dst,
+                      const screen_yz*restrict along,
+                      unsigned x0, unsigned x1,
+                      signed yoff) {
+  unsigned x, off;
+  coord_offset y0, y1, yl, yh, y;
+
+  for (x = x0; x < x1; ++x) {
+    y0 = along[x].y + yoff;
+    y1 = along[x+1].y + yoff;
+    if (y0 < y1) {
+      yl = (y0 > 0? y0 : 0);
+      yh = (y1 < (signed)dst->h? y1 : (signed)dst->h-1);
+    } else {
+      yl = (y1 > 0? y0 : 0);
+      yh = (y0 < (signed)dst->h? y0 : (signed)dst->h-1);
+    }
+
+    for (y = yl; y <= yh; ++y) {
+      off = canvas_offset(dst, x, y);
+      dst->px[off] = LINE_COLOUR;
+      dst->depth[off] = along[x].z;
+    }
+  }
+}
+
+static void draw_line_with_thickness(canvas*restrict dst,
+                                     const screen_yz*restrict along,
+                                     unsigned x0, unsigned x1,
+                                     unsigned thickness) {
+  unsigned t;
+
+  draw_line(dst, along, x0, x1, 0);
+
+  for (t = 0; t < thickness && t < x1; ++t) {
+    draw_line(dst, along, x0+t, x1-t, +(signed)t);
+    draw_line(dst, along, x0+t, x1-t, -(signed)t);
+  }
+}
+
+static void draw_segments(canvas*restrict dst,
+                          const screen_yz*restrict front,
+                          const screen_yz*restrict back,
+                          unsigned thickness) {
+  unsigned x0, x1;
+
+  for (x0 = 0; x0 < dst->w; ++x0) {
+    if (back[x0].y >= front[x0].y) {
+      x1 = x0+1;
+      while (x1 < dst->w && back[x1].y >= front[x0].y)
+        ++x1;
+
+      draw_line_with_thickness(dst, back, x0, x1, thickness);
+      x0 = x1;
+    }
+  }
+}
+
+static void fill_area_between(canvas*restrict dst,
+                              const screen_yz*restrict front,
+                              const screen_yz*restrict back) {
+  coord_offset y, y0, y1;
+  unsigned x, off;
+
+  for (x = 0; x < dst->w; ++x) {
+    if (front[x].y > back[x].y) {
+      y0 = back[x].y;
+      y1 = front[x].y;
+      if (y0 < 0) y0 = 0;
+      if (y1 >= (signed)dst->h) y1 = (signed)dst->h-1;
+
+      for (y = y0; y <= y1; ++y) {
+        off = canvas_offset(dst, x, y);
+        dst->px[off] = texture[
+          TEXSZ*((y+front[x].y) & TEXMASK) +
+          (x & TEXMASK)];
+        dst->depth[off] = front[x].z;
+      }
+    }
+  }
+}
+
+void terrabuff_render(canvas*restrict dst,
+                      const terrabuff*restrict this,
+                      const rendering_context*restrict ctxt) {
+  const rendering_context_invariant*restrict context =
+    (const rendering_context_invariant*restrict)ctxt;
+  screen_yz lbuffa[dst->w+1], lbuffb[dst->w+1];
+  screen_yz*restrict lbuff_front = lbuffa, *restrict lbuff_back = lbuffb;
+  screen_yz* lbuff_tmp;
+  unsigned scan, x, line_thickness;
+
+  line_thickness = 1 + dst->w / 512;
+
+  /* Render from the bottom up. First, initialise the front-yz buffer to have
+   * the minimum Y coordinate at all points, so that the bottom level never
+   * interacts.
+   *
+   * Then, for each iteration, swap the yz buffers, and interpolate the new
+   * scan into the front buffer. Fill the areas where the front buffer Y is
+   * greater than the back buffer Y. Then draw line segments along the back
+   * buffer where its Y is greater than or equal to the front buffer.
+   *
+   * When done, draw a line across the entire front buffer to add contrast to
+   * the sky and to be consistent with how the rest of the drawing looks.
+   */
+  for (x = 0; x <= dst->w; ++x) {
+    lbuff_front[x].y = 0x80000000; /* most negative value */
+    lbuff_front[x].z = 0x7FFFFFFF;
+  }
+  for (scan = 0; scan < this->scan; ++scan) {
+    lbuff_tmp = lbuff_front;
+    lbuff_front = lbuff_back;
+    lbuff_back = lbuff_tmp;
+
+    interpolate_all(lbuff_front,
+                    this->points + this->scap*scan + this->boundaries[scan].low,
+                    this->boundaries[scan].high - this->boundaries[scan].low,
+                    dst->w);
+
+    fill_area_between(dst, lbuff_front, lbuff_back);
+    draw_segments(dst, lbuff_front, lbuff_back, line_thickness);
+  }
+
+  draw_line_with_thickness(dst, lbuff_front, 0, dst->w, line_thickness);
+}
---
+++ src/render/terrabuff.h
@@ -0,0 +1,113 @@
+/*-
+ * Copyright (c) 2013 Jason Lingle
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef RENDER_TERRABUFF_H_
+#define RENDER_TERRABUFF_H_
+
+#include "../coords.h"
+#include "../graphics/canvas.h"
+#include "../graphics/perspective.h"
+#include "context.h"
+
+/**
+ * The terrabuff is used for rendering large-scale terrain features. Rendering
+ * with a terrabuff is performed in terms of polar coordinates. Some fixed set
+ * of equally-spaced angles are chosen, which are invariant with respect to the
+ * perspective. The renderer then runs along each angle in order at a fixed
+ * distance, telling the terrabuff about features found within each scanned
+ * area; after each scan, that distance increases.
+ *
+ * The terrabuff is indifferent to the algorithm chosen for the distance, as
+ * long as it is strictly increasing. Slices must be presented in incremental
+ * order, modulo the buffer slice capacity.
+ *
+ * The terrabuff is also limited as to how many scans it can hold. The client
+ * is not required to fill all scans, but must not exceed them.
+ *
+ * Once all data have been fed into a terrabuff, terrabuff_render() may be
+ * called to write the data onto a canvas.
+ */
+typedef struct terrabuff_s terrabuff;
+/**
+ * Unsigned integer type indicating terrabuff slice indices.
+ */
+typedef unsigned short terrabuff_slice;
+
+/**
+ * Initialises static resources needed for terrabuff rendering. This should be
+ * called exactly once before any other terrabuff functions.
+ */
+void terrabuff_init(void);
+
+/**
+ * Allocates a new terrabuff with capacity for the given number of
+ * slices and scans. slice_capacity MUST be a power of two. scan_capacity is
+ * unrestricted.
+ */
+terrabuff* terrabuff_new(terrabuff_slice slice_capacity,
+                         unsigned scan_capacity);
+/**
+ * Frees the memory held by the given terrabuff.
+ */
+void terrabuff_delete(terrabuff*);
+
+/**
+ * Makes the terrabuff ready for a new rendering frame. All buffered data are
+ * erased. The buffer will begin scanning from slice l (inclusive) to h
+ * (exclusive).
+ */
+void terrabuff_clear(terrabuff*, terrabuff_slice l, terrabuff_slice h);
+/**
+ * Prepares the terrabuff to render the next scan. This MUST only be called
+ * after a terrabuff_put() which filled the previous scan. l and h are set to
+ * the low (inclusive) and high (exclusive) indices which are to be used for
+ * the subsequent scan.
+ */
+void terrabuff_next(terrabuff*, terrabuff_slice* l, terrabuff_slice* h);
+
+/**
+ * Informs the terrabuff of the orthogonal screen coordinates of a terrain
+ * feture for the given scan/slice pair, incrementing the slice. xmax is the
+ * maximum (exclusive) X coordinate of the drawable area.
+ */
+void terrabuff_put(terrabuff*, const vo3 where, coord_offset xmax);
+
+/**
+ * Renders the buffered terrain into the given canvas.
+ *
+ * This function assumes it is the first thing to render to the canvas, and
+ * largely ignores the depth buffer. Z coordinates generally reflect the
+ * back-most feature of any terrain segment. This is cheaper to store and
+ * render, and is safe as nothing is ever rendered under the terrain. It also
+ * reduces the potential for clipping effects due to things drawn near the
+ * terrain.
+ */
+void terrabuff_render(canvas*restrict,
+                      const terrabuff*restrict,
+                      const rendering_context*restrict);
+
+#endif /* RENDER_TERRABUFF_H_ */
