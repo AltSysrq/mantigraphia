fd81038e80e1f7cc6069e8553607d61db340ae1c
Jason Lingle <jason@lin.gl>
2014-02-16 21:17:29 -0700

    Make X offsets chaotic in GL-based terrabuff rendering.

--- share/glsl/vterrabuff.glsl
+++ share/glsl/vterrabuff.glsl
@@ -2,12 +2,17 @@ attribute float side;
 varying float rel;
 varying vec2 screen_coords;
 uniform float xoff;
+uniform vec2 screen_size;
 
 void main() {
   gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
   gl_TexCoord[0] = gl_MultiTexCoord0;
   gl_FrontColor = gl_Color;
   gl_FrontSecondaryColor = gl_SecondaryColor;
-  screen_coords = vec2(gl_Vertex.x + xoff, gl_Vertex.y);
+  screen_coords = vec2(gl_Vertex.x + xoff +
+                       screen_size.x / 4 * 128 *
+                       (31 * gl_MultiTexCoord0.t +
+                        75 * gl_MultiTexCoord0.t * gl_MultiTexCoord0.t),
+                       gl_Vertex.y);
   rel = side;
 }
