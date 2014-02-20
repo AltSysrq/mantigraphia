697166d390213e550bb01574c5dd35859a96a3a5
Jason Lingle <jason@lin.gl>
2014-02-19 19:41:52 -0700

    Fix type error in fbrush.glsl.

--- share/glsl/fbrush.glsl
+++ share/glsl/fbrush.glsl
@@ -12,7 +12,7 @@ void main() {
   val = noise * val + (1.0f - noise);
   val -= decay * gl_TexCoord[0].t * dist_from_centre *
     texture2D(tex, vec2(gl_TexCoord[0].s + texoff,
-                        gl_TexCoord[0].t * 2.0f + 0.5f));
+                        gl_TexCoord[0].t * 2.0f + 0.5f)).r;
 
   if (val < 0) discard;
 
