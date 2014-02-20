ae2310ea81e7a2cb08f20dd2f80f22429e68f056
Jason Lingle <jason@lin.gl>
2014-02-19 20:40:48 -0700

    Draw tree branches with single stroke.

--- src/render/tree-props.c
+++ src/render/tree-props.c
@@ -48,7 +48,8 @@
 static lsystem temp_tree_system;
 
 void tree_props_init(void) {
-  /* 9..6: Branch lengths
+  /* 9..6: Branch lengths (movement)
+   * -   : Draw branch
    * A..H: Leaf splotches
    * [  ]: Push/pop
    * (  ): Push/pop with size reduction
@@ -69,7 +70,7 @@ void tree_props_init(void) {
     "E F",
     "F G",
     "G H",
-    ". . [(q9B)z.] [(r9B)z.] [(s9B)z.] [(t9B)z.] [(u9B)z.] [(v9B)z.]",
+    ". . [(q-9B)z.] [(r-9B)z.] [(s-9B)z.] [(t-9B)z.] [(u-9B)z.] [(v-9B)z.]",
     "z a b c d e f g h",
     NULL);
 }
@@ -127,7 +128,7 @@ static void render_tree_prop_temp(drawing_queue* queue, const world_prop* this,
   else if (level < 61) level = 6;
   else                 level = 6;
 
-  lsystem_execute(&sys, &temp_tree_system, "9A", level, this->x^this->z);
+  lsystem_execute(&sys, &temp_tree_system, "-9A", level, this->x^this->z);
 
   drawing_queue_start_burst(&burst, queue);
   dq_shared_fast_brush(&burst, context);
@@ -158,6 +159,17 @@ static void render_tree_prop_temp(drawing_queue* queue, const world_prop* this,
       --depth;
       break;
 
+    case '-':
+      turtle[depth+1] = turtle[depth];
+      turtle_move(turtle+depth+1, 0,
+                  (5 * METRE >> size_shift)
+                  / TURTLE_UNIT,
+                  0);
+      turtle_put_draw_line(&burst, turtle+depth+1,
+                           METRE >> size_shift, METRE >> size_shift,
+                           screen_width);
+      break;
+
     case '9':
     case '8':
     case '7':
@@ -166,9 +178,6 @@ static void render_tree_prop_temp(drawing_queue* queue, const world_prop* this,
                   (5 * METRE >> ('9' - sys.buffer[i]) >> size_shift)
                   / TURTLE_UNIT,
                   0);
-      turtle_put_draw_line(&burst, turtle+depth,
-                           METRE >> size_shift, METRE >> size_shift,
-                           screen_width);
       break;
 
     case 'a':
