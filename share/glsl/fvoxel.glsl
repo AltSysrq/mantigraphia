2183c77fb9fbc536175b7dee5c062fcd9e6b21b9
Jason Lingle <jason@lin.gl>
2015-01-08 20:29:15 -0700

    Fix fvoxel.glsl on Mesa GL.
    Apparently `input` is a reserved word there.

--- share/glsl/fvoxel.glsl
+++ share/glsl/fvoxel.glsl
@@ -2,14 +2,14 @@ uniform sampler2D tex, palette;
 uniform float palette_t;
 
 void main() {
-  vec3 input;
+  vec3 tinput;
   vec4 colour;
 
-  input = texture2D(tex,  gl_TexCoord[0].st).rgb;
-  colour = texture2D(palette, vec2(input.r, palette_t));
+  tinput = texture2D(tex,  gl_TexCoord[0].st).rgb;
+  colour = texture2D(palette, vec2(tinput.r, palette_t));
 
-  if (colour.a < input.b) discard;
+  if (colour.a < tinput.b) discard;
 
   gl_FragColor.rgb = colour.rgb;
-  gl_FragColor.a = input.g;
+  gl_FragColor.a = tinput.g;
 }
