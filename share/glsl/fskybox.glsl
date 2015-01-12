896bc6b51c8e2e6ad3ce372c5c22c132f9593d7a
Jason Lingle <jason@lin.gl>
2015-01-11 21:27:52 -0700

    Move ray computations for skybox clouds to vertex shader.

--- share/glsl/fskybox.glsl
+++ share/glsl/fskybox.glsl
@@ -1,11 +1,8 @@
 uniform sampler2D clouds;
-uniform vec2 yrot;
-uniform vec2 rxrot;
 uniform vec2 cloud_offset_1;
 uniform vec2 cloud_offset_2;
 
-varying vec2 screen_off;
-varying float scale;
+varying vec3 ray;
 
 const float METRE = 65536.0f;
 const float EARTH_RADIUS = 6378.1e1 * METRE;
@@ -15,22 +12,9 @@ const float CLOUD_VISIBILITY = 8192 * METRE;
 const float CLOUD_SCALE = 4096 * METRE;
 
 void main() {
-  mat3 camera_y, camera_rx;
-  vec3 ray;
+  vec3 norm_ray = normalize(ray);
 
-  camera_y = mat3(vec3(-yrot.x, 0.0f, yrot.y),
-                  vec3(0.0f, 1.0f, 0.0f),
-                  vec3(-yrot.y, 0.0f, -yrot.x));
-  camera_rx = mat3(vec3(1.0f, 0.0f, 0.0f),
-                   vec3(0.0f, rxrot.x, -rxrot.y),
-                   vec3(0.0f, rxrot.y, rxrot.x));
-
-  ray = normalize(
-    camera_y *
-    camera_rx *
-    vec3(screen_off.x / scale, -screen_off.y / scale, -1.0f));
-
-  if (ray.y <= 0.0f) {
+  if (norm_ray.y <= 0.0f) {
     gl_FragColor = gl_Color;
     return;
   }
@@ -43,12 +27,12 @@ void main() {
    * cloud colour is blended into the base sky colour based on the distance.
    */
   vec3 centre_of_earth_to_camera = vec3(0.0f, EARTH_RADIUS, 0.0f);
-  vec3 projected = ray * dot(centre_of_earth_to_camera, ray);
+  vec3 projected = norm_ray * dot(centre_of_earth_to_camera, norm_ray);
   float centre_offset = sqrt(
     CLOUD_RADIUS * CLOUD_RADIUS -
     pow(distance(projected, centre_of_earth_to_camera), 2.0f));
   float distance_to_intersect = centre_offset - length(projected);
-  vec3 intersection = ray * distance_to_intersect;
+  vec3 intersection = norm_ray * distance_to_intersect;
 
   float cloud = distance_to_intersect < CLOUD_VISIBILITY?
     (texture2D(clouds, intersection.xz / CLOUD_SCALE + cloud_offset_1).r +
--- share/glsl/vskybox.glsl
+++ share/glsl/vskybox.glsl
@@ -1,16 +1,31 @@
 uniform vec2 screen_size;
 uniform float fov;
+uniform vec2 yrot;
 uniform vec2 rxrot;
 
-varying vec2 screen_off;
 varying float scale;
+varying vec3 ray;
 
 void main() {
   mat3 camera_y, camera_rx;
+  vec2 screen_off;
   float angle;
 
   screen_off = gl_Vertex.xy - screen_size / 2.0f;
   scale = screen_size.x * tan(fov / 2.0f) / 2.0f;
+
+  camera_y = mat3(vec3(-yrot.x, 0.0f, yrot.y),
+                  vec3(0.0f, 1.0f, 0.0f),
+                  vec3(-yrot.y, 0.0f, -yrot.x));
+  camera_rx = mat3(vec3(1.0f, 0.0f, 0.0f),
+                   vec3(0.0f, rxrot.x, -rxrot.y),
+                   vec3(0.0f, rxrot.y, rxrot.x));
+
+  ray = normalize(
+    camera_y *
+    camera_rx *
+    vec3(screen_off.x / scale, -screen_off.y / scale, -1.0f));
+
   angle = rxrot + screen_off.y * fov / screen_size.x +
     3.14159f / 2.0f;
 
