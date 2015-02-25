5cb9d110d01072c4664775a1ca028102c06962a9
Jason Lingle <jason@lin.gl>
2015-02-24 19:56:42 -0700

    Include z coordinate in manifold texture coordinates.

--- share/glsl/vmanifold.glsl
+++ share/glsl/vmanifold.glsl
@@ -12,6 +12,7 @@ void main() {
   proj = perspective_proj(gl_Vertex);
   gl_Position = gl_ModelViewProjectionMatrix * proj;
   texc = gl_Vertex.xy / METRE / 4.0f;
+  texc.s += gl_Vertex.z / METRE / 4.0f;
   texc *= noise_freq;
   gl_TexCoord[0] = vec4(texc.s, texc.t, 0.0f, 1.0f);
   scaled_noise_amplitude = min(1.0f, noise_amplitude -
