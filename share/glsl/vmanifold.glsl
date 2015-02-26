462b2bf7871d394b1dba7b604853d2cb12fe42a6
Jason Lingle <jason@lin.gl>
2015-02-25 20:58:01 -0700

    Improve manifold texture coordinate generation.

--- share/glsl/vmanifold.glsl
+++ share/glsl/vmanifold.glsl
@@ -13,6 +13,7 @@ void main() {
   gl_Position = gl_ModelViewProjectionMatrix * proj;
   texc = gl_Vertex.xy / METRE / 4.0f;
   texc.s += gl_Vertex.z / METRE / 4.0f;
+  texc.t += cos(gl_Vertex.z / METRE / 16.0f * 3.14159f);
   texc *= noise_freq;
   gl_TexCoord[0] = vec4(texc.s, texc.t, 0.0f, 1.0f);
   scaled_noise_amplitude = noise_amplitude - noise_amplitude * clamp(
