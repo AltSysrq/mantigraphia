b30441ce3d0cb735feaf12dc1d665a59c4d39606
Jason Lingle <jason@lin.gl>
2015-03-29 12:22:18 -0600

    Parallelise lightweight world object distribution.
    Load time: 14s -> 8s

--- src/world/world-object-distributor.c
+++ src/world/world-object-distributor.c
@@ -31,6 +31,7 @@
 
 #include "../alloc.h"
 #include "../defs.h"
+#include "../micromp.h"
 #include "../math/coords.h"
 #include "../math/rand.h"
 #include "terrain-tilemap.h"
@@ -143,28 +144,29 @@ unsigned wod_add_flower(flower_type type, coord h0, coord h1) {
 
 static unsigned long long wod_distribute_subregion(
   unsigned max_instances, unsigned threshold,
-  coord x0, coord z0, coord xmask, coord zmask
+  coord x0, coord z0, coord xmask, coord zmask,
+  mersenne_twister* twister
 ) {
   unsigned long long cost = max_instances;
   unsigned attempt, subsample, subsamples = 0, x, z, w, h, off, type;
 
-  if (!wod_num_elements)
-    return 0;
-
   for (attempt = 0; attempt < max_instances; ++attempt) {
-    type = twist(&wod_twister) % wod_num_elements;
+    type = twist(twister) % wod_num_elements;
     switch (wod_elements[type].type) {
     case wodet_ntvp:
       subsamples = 1;
       break;
     case wodet_flower:
       subsamples = 32;
+      /* If not light-weight, charge the caller for mixing types */
+      /* (If light-weight, this increment is ultimately ignored) */
+      cost += 31;
       break;
     }
 
     for (subsample = 0; subsample < subsamples; ++subsample) {
-      x = x0 + (twist(&wod_twister) & xmask);
-      z = z0 + (twist(&wod_twister) & zmask);
+      x = x0 + (twist(twister) & xmask);
+      z = z0 + (twist(twister) & zmask);
       off = terrain_tilemap_offset(wod_terrain, x, z);
 
       if (!wod_permitted_terrain[wod_terrain->type[off] >> TERRAIN_SHADOW_BITS]
@@ -187,10 +189,10 @@ static unsigned long long wod_distribute_subregion(
 
       case wodet_flower:
         h = wod_elements[type].v.flower.minh +
-          twist(&wod_twister) % wod_elements[type].v.flower.hrange;
+          twist(twister) % wod_elements[type].v.flower.hrange;
         flower_map_put(wod_flowers, wod_elements[type].v.flower.type, h,
-                       x * TILE_SZ + (twist(&wod_twister) % TILE_SZ),
-                       z * TILE_SZ + (twist(&wod_twister) % TILE_SZ));
+                       x * TILE_SZ + (twist(twister) % TILE_SZ),
+                       z * TILE_SZ + (twist(twister) % TILE_SZ));
         break;
       }
     }
@@ -199,6 +201,9 @@ static unsigned long long wod_distribute_subregion(
   return cost;
 }
 
+/**
+ * This needs to be a multiple of FLOWER_FHIVE_SIZE for thread-safety.
+ */
 #define SUBREGION_SIZE 64
 
 static unsigned long long wod_distribute_serial(
@@ -214,15 +219,95 @@ static unsigned long long wod_distribute_serial(
     for (x = 0; x < nx; ++x)
       cost += wod_distribute_subregion(max_instances / nx / nz, threshold,
                                        x * SUBREGION_SIZE, z * SUBREGION_SIZE,
-                                       SUBREGION_SIZE-1, SUBREGION_SIZE-1);
+                                       SUBREGION_SIZE-1, SUBREGION_SIZE-1,
+                                       &wod_twister);
 
   return cost;
+}
+
+static unsigned wod_distribute_ump_max_instances;
+static unsigned wod_distribute_ump_threshold;
+static unsigned* wod_distribute_ump_twister_seeds;
+
+static void wod_distribute_in_ump(unsigned ordinal, unsigned total) {
+  mersenne_twister twister;
+  unsigned x, z, nx, nz;
+
+  twister_seed(&twister, wod_distribute_ump_twister_seeds[ordinal]);
+
+  nx = wod_terrain->xmax / SUBREGION_SIZE;
+  nz = wod_terrain->zmax / SUBREGION_SIZE;
+  z = ordinal;
+
+  for (x = 0; x < nx; ++x) {
+    wod_distribute_subregion(wod_distribute_ump_max_instances / nx / nz,
+                             wod_distribute_ump_threshold,
+                             x * SUBREGION_SIZE, z * SUBREGION_SIZE,
+                             SUBREGION_SIZE-1, SUBREGION_SIZE-1,
+                             &twister);
+  }
+}
+
+static ump_task wod_distribute_ump_task = {
+  wod_distribute_in_ump,
+  /* set dynamically */ 0,
+  /* sync */ 0
+};
+
+static unsigned long long wod_distribute_parallel(
+  unsigned max_instances, unsigned threshold
+) {
+  unsigned nz, i;
+
+  /* Split into rows of subregions and process them in parallel */
+  nz = wod_terrain->zmax / SUBREGION_SIZE;
+  wod_distribute_ump_max_instances = max_instances;
+  wod_distribute_ump_threshold = threshold;
+  wod_distribute_ump_task.num_divisions = nz;
+  /* Generate a seed for each row so it can use its own private twister */
+  wod_distribute_ump_twister_seeds = xmalloc(sizeof(unsigned) * nz);
+  for (i = 0; i < nz; ++i)
+    wod_distribute_ump_twister_seeds[i] = twist(&wod_twister);
+
+  ump_run_sync(&wod_distribute_ump_task);
+  free(wod_distribute_ump_twister_seeds);
+
+  /* All lightweight elements are cost 1 */
+  return max_instances;
+}
+
+/**
+ * Returns whether the current set of types is lightweight; ie, can be
+ * parallelised.
+ *
+ * All lightweight types have cost 1 when processed in lightweight mode.
+ */
+static int wod_is_lightweight(void) {
+  unsigned i;
+
+  for (i = 0; i < wod_num_elements; ++i) {
+    switch (wod_elements[i].type) {
+    case wodet_ntvp:
+      /* May trigger uMP calls */
+      return 0;
+
+    default: /* lightweight */ break;
+    }
+  }
+
+  return 1;
 }
 
 unsigned wod_distribute(unsigned max_instances, unsigned threshold) {
   unsigned long long cost;
 
-  cost = wod_distribute_serial(max_instances, threshold);
+  if (!wod_num_elements)
+    return 0;
+
+  if (wod_is_lightweight())
+    cost = wod_distribute_parallel(max_instances, threshold);
+  else
+    cost = wod_distribute_serial(max_instances, threshold);
 
   return cost > 0xFFFFFFFFLL? ~0u : cost;
 }
