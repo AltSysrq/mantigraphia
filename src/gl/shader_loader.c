ad521702dce98878867401ff9aab89851abf574b
Jason Lingle <jason@lin.gl>
2015-04-04 22:14:29 -0600

    Downgrade GLSL version to 1.30 (OpenGL 3.0).
    Bah, Intel strikes again.

--- src/gl/shader_loader.c
+++ src/gl/shader_loader.c
@@ -36,7 +36,7 @@
 
 #include "shader_loader.h"
 
-static const char glsl_version[] = "#version 140\n";
+static const char glsl_version[] = "#version 130\n";
 
 void load_shader(GLuint* shader, const char* basename) {
   static char buffer[65536];
